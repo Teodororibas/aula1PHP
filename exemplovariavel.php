ce1a99111c7d8baad4a68171a2fb9ed92ad062fe
Teodororibas <teodororibas@icloud.com>
2024-10-30 16:54:31 -0400

    tesk01

---
+++ exemplovariavel.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "enilda";
+    $sobrenome = "caceres";
+    $idade = "41";
+    $profissao = "professora";
+
+    echo "oi $profissao $nome $sobrenome. <br>";
+    echo "voce possui atualmente $idade anos.";
+?>
